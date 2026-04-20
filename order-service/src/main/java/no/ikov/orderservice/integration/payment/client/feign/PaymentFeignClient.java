3645195ecc8c3fd6f243ea0daab97eff9636824a
ik0v <ivan.kovals@hotmail.com>
2026-04-20 17:32:34 +0200

    open api dependency and configuration added to all services

--- order-service/src/main/java/no/ikov/orderservice/integration/payment/client/feign/PaymentFeignClient.java
+++ order-service/src/main/java/no/ikov/orderservice/integration/payment/client/feign/PaymentFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "payment-service", url = "http://localhost:8082/api/payments")
+@FeignClient(name = "payment-service", url = "${payment.service.url}/api/payments")
 public interface PaymentFeignClient {
 
     @PostMapping
